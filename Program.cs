eb31a491108d677e4269a8538d683a1e704c58e1
Vladyslav <vladurlov17@gmail.com>
2025-09-25 13:48:30 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+    
+        string[] parts = Console.ReadLine().Split();
+        if (parts[0][0] == parts[1][0])
+            Console.WriteLine("Last name begins with the same letter as first name");
+        else
+            Console.WriteLine("The last name does not start with the same letter as the first name");
+    
